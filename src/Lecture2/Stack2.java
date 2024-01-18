1e91e58a13037310d5692ca388e68eb250f3fb7c
andrewcsu <andrewsu@unc.edu>
2024-01-18 15:47:39 -0500

    Initial commit

---
+++ src/Lecture2/Stack2.java
@@ -0,0 +1,11 @@
+package Lecture2;
+
+public class Stack2 {
+    public static void main (String[] args) {
+        int n = 0;
+        n1();
+    }
+    public static void n1 () {
+        int n1 = 1;
+    }
+}
